d655340a01f379c818d59a714d0b928fd3fc1bf8
Abhishek joji <148741327+AJ26WEST@users.noreply.github.com>
2024-09-26 21:23:02 +0530

    Update tomo.py

--- tomo.py
+++ tomo.py
@@ -1,1 +1,7 @@
-fien
+import datetime
+
+# Get the current date and time
+now = datetime.datetime.now()
+
+# Print the day and date
+print("Today is", now.strftime("%A"), "and the date is", now.strftime("%B %d, %Y"))
